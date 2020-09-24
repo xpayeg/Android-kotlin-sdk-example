21864561eac5ff9b01cf20166fed49d7a80c45fb
Mahmoud Aziz <mabdelaziz@xpaytech.co>
2020-09-24 20:56:15 +0200

    add custom view for kiosk payment

--- app/src/main/java/com/xpay/kotlin/PayActivity.kt
+++ app/src/main/java/com/xpay/kotlin/PayActivity.kt
@@ -7,11 +7,10 @@ import android.os.Bundle
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.ProgressBar
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.xpay.kotlinutils.XpayUtils
 import dmax.dialog.SpotsDialog
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_pay.*
 
 
@@ -22,17 +21,31 @@ class PayActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pay)
         dialog = SpotsDialog.Builder().setContext(this).build()
-        dialog?.show()
-        webView2.loadUrl(XpayUtils.iframeUrl)
-        webView2.webViewClient = object : WebViewClient() {
-            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+        if(XpayUtils.iframeUrl!=null){
+            dialog?.show()
+            showHide(webView2)
+            webView2.loadUrl(XpayUtils.iframeUrl)
+            webView2.webViewClient = object : WebViewClient() {
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
 
-            }
+                }
 
-            override fun onPageFinished(view: WebView, url: String) {
-                showHide(fab)
-                dialog?.hide()
+                override fun onPageFinished(view: WebView, url: String) {
+                    showHide(fab)
+                    dialog?.hide()
+                }
             }
+        }else{
+            val uuid = intent.getStringExtra("UUID")
+            val message = intent.getStringExtra("MESSAGE")
+            txtUid.text=uuid
+            txtMsg.text=message
+        }
+
+        btnDone.setOnClickListener{
+            val intent = Intent(this, Login::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
         }
         fab.setOnClickListener {
             val intent = Intent(this, Login::class.java)
