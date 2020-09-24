399d3dee4d5f701ea06cc01cba79f42e2975b96e
Mahmoud Aziz <mabdelaziz@xpaytech.co>
2020-09-24 20:55:48 +0200

    confirm dada before payment process

--- app/src/main/java/com/xpay/kotlin/MainActivity.kt
+++ app/src/main/java/com/xpay/kotlin/MainActivity.kt
@@ -11,32 +11,42 @@ import dmax.dialog.SpotsDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var dialog: AlertDialog? =null
+    var dialog: AlertDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        dialog= SpotsDialog.Builder().setContext(this@MainActivity).build()
+        dialog = SpotsDialog.Builder().setContext(this@MainActivity).build()
 
-          textName.text="Name: \n${XpayUtils.user!!.name}"
-          textEmail.text="Email: \n${XpayUtils.user!!.email}"
-          textView3.text="Phone: \n+2${XpayUtils.user?.phone}"
-          textView4.text="Connunity ID: \n${XpayUtils.communityId}"
-          textVariableID.text="Variable ID: \n${XpayUtils.variableAmountID}"
+        textName.text = "Name: \n${XpayUtils.user!!.name}"
+        textEmail.text = "Email: \n${XpayUtils.user!!.email}"
+        txtPhone.text = "Phone: \n+2${XpayUtils.user?.phone}"
+        txtCommunity.text = "Connunity ID: \n${XpayUtils.communityId}"
+        textVariableID.text = "Variable ID: \n${XpayUtils.variableAmountID}"
+        txtMethod.text = "Payment Method: \n${XpayUtils.payUsing}"
+        totalAmount.text = "Total Amount: \n${XpayUtils.amount}"
 
-        doneButton.setOnClickListener{
+        doneButton.setOnClickListener {
             val totalAmount = getIntent().getStringExtra("TOTAL_AMOUNT")?.toDouble()
             if (totalAmount != null) {
                 dialog!!.show()
-                XpayUtils.pay( String.format("%.2f", totalAmount).toDouble(),XpayUtils.apiKey!!,::userSuccess,::userFailure)
+                XpayUtils.pay(::userSuccess, ::userFailure)
             }
         }
 
     }
+
     fun userSuccess(res: PayResponse) {
-        XpayUtils.iframeUrl=res.data.iframe_url
-        startActivity(Intent(this, PayActivity::class.java))
         dialog?.dismiss()
+        XpayUtils.iframeUrl = res.data.iframe_url
+        if (res.data.iframe_url != null) {
+            startActivity(Intent(this, PayActivity::class.java))
+        } else if (res.data.message != null) {
+            val intent = Intent(baseContext, PayActivity::class.java)
+            intent.putExtra("UUID", res.data.transaction_uuid)
+            intent.putExtra("MESSAGE", res.data.message)
+            startActivity(intent)
+        }
     }
 
     fun userFailure(res: String) {
