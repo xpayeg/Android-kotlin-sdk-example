55066e97762c88db88f3c5b610b9ac4e9b6e110a
Islam Rostom <islam.rostom93@gmail.com>
2020-11-17 19:41:34 +0200

    update payactivity

--- app/src/main/java/com/xpay/kotlin/PayActivity.kt
+++ app/src/main/java/com/xpay/kotlin/PayActivity.kt
@@ -1,49 +1,28 @@
 package com.xpay.kotlin
 
-import android.app.AlertDialog
-import android.content.ComponentName
 import android.content.Intent
-import android.graphics.Color
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.browser.customtabs.*
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.core.content.ContextCompat
 import com.xpay.kotlinutils.XpayUtils
-import dmax.dialog.SpotsDialog
 import kotlinx.android.synthetic.main.activity_pay.*
 
 
 class PayActivity : AppCompatActivity() {
-    var uuid: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_pay)
-        if (XpayUtils.ShippingInfo != null) {
-            txt_status.text = "Successful Payment"
 
-        }
-        uuid = intent.getStringExtra("UUID")
+        val uuid = intent.getStringExtra("UUID")
         val message = intent.getStringExtra("MESSAGE")
         txtUid.text = uuid
         txtMsg.text = message
 
         btnDone.setOnClickListener {
-            goToTransaction(uuid!!)
+            val intent = Intent(this, Login::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
         }
 
-    }
-
-    // go to transaction detail screen
-    private fun goToTransaction(Uid: String) {
-        val intent = Intent(this, TransactionActivity::class.java)
-        intent.putExtra("UUID", Uid)
-        startActivity(intent)
     }
 
 }
