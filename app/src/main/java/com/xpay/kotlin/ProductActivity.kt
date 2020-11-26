fff7b9be32fc8fba2fd8023f737a17dc52b0ef76
Islam Rostom <islam.rostom93@gmail.com>
2020-11-26 13:22:10 +0200

    Update ProductActivity.kt

--- app/src/main/java/com/xpay/kotlin/ProductActivity.kt
+++ app/src/main/java/com/xpay/kotlin/ProductActivity.kt
@@ -6,8 +6,6 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.chip.Chip
-import com.xpay.kotlinutils.XpayUtils
-import kotlinx.coroutines.launch
 import dmax.dialog.SpotsDialog
 import kotlinx.android.synthetic.main.activity_product.*
 
